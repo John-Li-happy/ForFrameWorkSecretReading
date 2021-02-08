ccd1a354dddcaca82cc7bdf85c39ea330be3972d
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2021-02-08 18:04:12 -0500

    init commit

---
+++ ForFrameWorkSecretReading/FrameWork/SecretsFrameWork.framework/Headers/SecretsFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  SecretsFrameWork.h
+//  SecretsFrameWork
+//
+//  Created by Zhaoyang Li on 2/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecretsFrameWork.
+FOUNDATION_EXPORT double SecretsFrameWorkVersionNumber;
+
+//! Project version string for SecretsFrameWork.
+FOUNDATION_EXPORT const unsigned char SecretsFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecretsFrameWork/PublicHeader.h>
+
+
